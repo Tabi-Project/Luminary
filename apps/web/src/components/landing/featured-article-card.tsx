42c4bfa582c466642739a4dbd93680d5d9c5b0d4
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-06-22 17:20:14 +0100

    built out card for displaying short information selected articles

---
+++ apps/web/src/components/landing/featured-article-card.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/utils/cn";
+import { capitalizeFirstLetter } from "@/utils/string";
+
+interface FeaturedArticleCardProps {
+  title: string;
+  tags: string[];
+  author: string;
+  className?: string;
+}
+
+export function FeaturedArticleCard({
+  title,
+  tags,
+  author,
+  className,
+}: FeaturedArticleCardProps) {
+  return (
+    <div
+      className={cn(
+        "w-full h-62.5 flex flex-col gap-3 justify-between p-6 bg-muted-blue rounded-xl",
+        className,
+      )}
+    >
+      <h2 className="text-2xl font-bold line-clamp-4">{title}</h2>
+      <div>
+        <div className="flex flex-wrap gap-2 mb-1">
+          {tags.map((tag) => (
+            <span
+              key={tag}
+              className="bg-light-gray text-text-main rounded-md px-2 py-1 text-sm font-semibold"
+            >
+              {capitalizeFirstLetter(tag)}
+            </span>
+          ))}
+        </div>
+        <p className="text-muted font-light">{author}</p>
+      </div>
+    </div>
+  );
+}
