64d3b64ba7d8c04369790e316032bec7e675b8d0
Ramnan Ramyil <ramyilramnan@gmail.com>
2026-06-24 13:58:58 +0100

    chore: change image host url for profile images

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "i.pravatar.cc",
+        hostname: "srglhfhutvxpfudrepyz.supabase.co",
         port: "",
       },
     ],
