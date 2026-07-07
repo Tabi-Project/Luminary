2af8d6b544f8d0f978781e7bcfce1ab9adeefc54
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-07-08 00:08:15 +0100

    fetching the 3 latest articles from sanity, improved styling on the article cards, added error state to the section

--- apps/web/src/components/landing/featured-article-card.tsx
+++ apps/web/src/components/landing/featured-article-card.tsx
@@ -17,23 +17,23 @@ export function FeaturedArticleCard({
   return (
     <div
       className={cn(
-        "w-full h-62.5 flex flex-col gap-3 justify-between p-6 bg-muted-blue rounded-xl",
+        "w-full h-fit sm:h-62.5 flex flex-col gap-3 justify-between p-6 bg-muted-blue rounded-xl",
         className,
       )}
     >
       <h2 className="text-2xl font-bold line-clamp-4">{title}</h2>
       <div>
-        <div className="flex flex-wrap gap-2 mb-1">
+        <div className="flex overflow-hidden gap-2 mb-1">
           {tags.map((tag) => (
             <span
               key={tag}
-              className="bg-light-gray text-text-main rounded-md px-2 py-1 text-sm font-semibold"
+              className="bg-light-gray text-text-main rounded-md px-2 py-1 text-sm font-semibold truncate"
             >
               {capitalizeFirstLetter(tag)}
             </span>
           ))}
         </div>
-        <p className="text-muted font-light">{author}</p>
+        <p className="text-muted font-light truncate">{author}</p>
       </div>
     </div>
   );
--- apps/web/src/components/landing/sections/featured-article-section.tsx
+++ apps/web/src/components/landing/sections/featured-article-section.tsx
@@ -1,45 +1,50 @@
 import { Button } from "@/components/common/button";
 import Link from "next/link";
 import { FeaturedArticleCard } from "../featured-article-card";
+import { Article } from "@/types/articles.type";
+import { fetchArticlesFromSantry } from "@/actions/sanity";
 
-export function FeaturedArticleSection() {
-  const articles = [
-    {
-      title: "Women in African Finance Today",
-      tags: ["Technology", "Africa"],
-      author: "Luminary",
-    },
-    {
-      title:
-        "An Assessment of Geo-Political Representation of Women in the 2023 Elections in Nigeria: The Role of the Media",
-      tags: ["Leadership", "Africa"],
-      author: "Author Name",
-    },
-    {
-      title:
-        "Northwood Space Secures $100M Series B and $50M Space Force Contract",
-      tags: ["Technology", "Africa"],
-      author: "Luminary",
-    },
-  ];
+export async function FeaturedArticleSection() {
+  const articles: Article[] | null = await fetchArticlesFromSantry();
 
   return (
     <section className="w-full">
       <div className="w-full flex items-center justify-between">
-        <span className="text-4xl font-bold">Featured Stories</span>
+        <span className="text-2xl sm:text-4xl font-bold shrink-0">
+          Featured Stories
+        </span>
         <Link href="/articles">
-          <Button text="View all stories" />
+          <Button text="View all stories" variant="ghost" />
         </Link>
       </div>
-      <div className="w-full grid grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
-        {articles.map((article, idx) => (
-          <FeaturedArticleCard
-            {...article}
-            key={idx}
-            className={idx === 2 ? "col-span-2 lg:col-span-1" : ""}
-          />
-        ))}
-      </div>
+      {!articles ? (
+        <ErrorState />
+      ) : (
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
+          {articles.slice(0, 3).map((article, idx) => (
+            <FeaturedArticleCard
+              key={idx}
+              title={article.title}
+              author={article.author}
+              tags={[article.field, article.source]}
+              className={idx === 2 ? "sm:col-span-2 lg:col-span-1" : ""}
+            />
+          ))}
+        </div>
+      )}
     </section>
+  );
+}
+
+function ErrorState() {
+  return (
+    <div className="w-full h-62.5 border border-red-400 bg-red-100 rounded-2xl flex flex-col items-center justify-center mt-4">
+      <span className="text-3xl font-bold text-red-600">
+        Failed to load articles
+      </span>
+      <span className="text-sm font-medium text-red-600">
+        Please try again later
+      </span>
+    </div>
   );
 }
